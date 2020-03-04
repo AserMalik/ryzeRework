106e9ebe4970bfee0716eea3fa1e26f7de80e8a2
Aser Malik <asermalik@live.com>
2020-03-04 02:44:10 -0500

    added chart, reworked json response, added single img load stress tests

---
+++ src/components/chart.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+var initialState = {
+  labels: [1, 2, 3, 4, 5, 6, 7],
+  datasets: [
+    {
+      label: "0",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      data: [65, 59, 80, 81, 56, 55, 40]
+    },
+    {
+      label: "1",
+      fillColor: "rgba(151,187,205,0.2)",
+      strokeColor: "rgba(151,187,205,1)",
+      pointColor: "rgba(151,187,205,1)",
+      pointStrokeColor: "#fff",
+      data: [28, 48, 40, 19, 86, 27, 90]
+    }
+  ]
+};
+
+class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  componentDidMount() {
+    var _this = this;
+    var newState = initialState;
+    // returns a Chart.js instance reference
+    setInterval( () => {
+      var newData = [];
+      for (var i = 0; i < 100; i++) {
+        newData[i] = Math.random() * 100 + i;
+      }
+      newState.datasets[0].data = newData;
+      console.log("_this variable[0]: " + _this.state.datasets[0].data[2]);
+      console.log("_this variable[1]: " + _this.state.datasets[1].data[2]);
+      console.log("IS variable[0]: " + initialState.datasets[0].data[2]);
+      console.log("IS variable[1]: " + initialState.datasets[1].data[2]);
+
+      _this.setState(initialState);
+    }, 1000);
+  }
+
+  render() {
+    return <Line data={this.state} />;
+  }
+}
+
+export default Chart;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
-//import fetch from 'isomorphic-unfetch'
+//zingcharts, dygraph, or react-chartjs
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Chart from "../components/chart"
 
 const IndexPage = () => {
   //http://localhost:8080/ping
   const [itemData, setItemData] = useState(0)
   useEffect( () => {
-    fetch(`http://itemcheck.herokuapp.com/ping`)
+    fetch(`https://itemcheck.herokuapp.com/ping`)
     .then(response => response.json()) // parse JSON from request
     .then(resultData => {
       setItemData(resultData)
@@ -22,20 +23,62 @@ const IndexPage = () => {
       <SEO title="Home" />
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
+      <p>Now go build something  great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
+      <Chart />
       <ul>
-        {Object.values(itemData).map((item, index) => {
-            return (
-              <li key={index}>
-                <p>{index}: {item[0]}, stats: {Object.values(item[1]).toString()}</p>
-              </li>
-            )
-          }
-      )}
+        {Object.values(itemData).map((itemObject, index) => {
+          return(
+            <li key={index}>
+              {itemObject[0]}
+              <ul>
+                {Object.entries(itemObject[1]).map((itemStats, subIndex) => {
+                  console.log(itemStats)
+                  return(
+                    <li key={subIndex}>
+                      {itemStats[0]}: {itemStats[1]}
+                    </li>
+                  )
+                })}
+              </ul>
+            </li>
+          )
+        })}
       </ul>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+      </div>
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
